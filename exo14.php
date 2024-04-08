abf1a29e6220ea40988dd1edd638999bc3efb341
Ehonam <164899950+Ehonam@users.noreply.github.com>
2024-04-08 09:16:00 +0200

    Update exo14.php

--- exo14.php
+++ exo14.php
@@ -5,11 +5,11 @@
 <h2>Résultat</h2>
 
 <?php
-$dateNaissance = "17-01-1985";
-$dateCourante = date("21-05-2018");
+$dateNaissance = "22-04-1983";
+$dateCourante = date("08-04-2024");
 $diff = date_diff(date_create($dateNaissance), date_create($dateCourante));
 
-$annees = $diff->format('%y');
+$annees = $diff->format('%y'); // date au format jj-mm-aaaa ou dd-mm-yyyy en anglais
 $mois = $diff->format('%m');
 $jours = $diff->format('%d');
 
