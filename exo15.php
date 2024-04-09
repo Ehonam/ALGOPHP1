bc8d0f67fa2ae6b8a1168f86deb2dc5a1b202d47
Ehonam <164899950+Ehonam@users.noreply.github.com>
2024-04-09 10:22:29 +0200

    Update exo15.php

--- exo15.php
+++ exo15.php
@@ -9,6 +9,29 @@ $p2 = new Personne("DUCHEMIN", "Alice", "1985-01-17") ;</p>
 
 <?php
 
+class Personne {
+    // attributs
+    public $nom;
+    public $prenom;
+    public $date_naissance;
+// initialisation des différentes valeurs
+    public function __construct($nom, $prenom, $date_naissance) {
+        $this->nom = $nom;
+        $this->prenom = $prenom;
+        $this->date_naissance = new DateTime($date_naissance);
+    }
+// creation getter et des setter
+    public function getAge() {
+        $dateCourante = new DateTime();
+        $ecart = $this->date_naissance->diff($dateCourante);
+        return $ecart->y;
+    }
+}
 
+$p1 = new Personne("DUPONT", "Michel", "1980-02-19");
+$p2 = new Personne("DUCHEMIN", "Alice", "1985-01-17");
+
+echo $p1->prenom . " " . $p1->nom . " a " . $p1->getAge() . " ans<br>";
+echo $p2->prenom . " " . $p2->nom . " a " . $p2->getAge() . " ans<br>";
 
 ?>
