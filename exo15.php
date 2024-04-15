c87a4b7de1c88430e399adf727fabf94ece8d9d9
Ehonam <164899950+Ehonam@users.noreply.github.com>
2024-04-15 09:26:03 +0200

    Update exo15.php

--- exo15.php
+++ exo15.php
@@ -1,36 +1,28 @@
-<h1>Exercice 15</h1>
-
-<p>Créer une classe Personne (nom, prénom et date de naissance).
-Instancier 2 personnes et afficher leurs informations : nom, prénom et âge.
-$p1 = new Personne("DUPONT", "Michel", "1980-02-19") ;
-$p2 = new Personne("DUCHEMIN", "Alice", "1985-01-17") ;</p>
-
-<h2>Résultat</h2>
 
 <?php
+    //instanciation des deux personnes
+$p1 = new Personne("DUPONT", "Michel", "1980-02-19");
+$p2 = new Personne("DUCHEMIN", "Alice", "1985-01-17");
 
 class Personne {
-    // attributs
+    // création des attributs
     public $nom;
     public $prenom;
     public $date_naissance;
-// initialisation des différentes valeurs
+    // initialisation des valeurs
     public function __construct($nom, $prenom, $date_naissance) {
         $this->nom = $nom;
         $this->prenom = $prenom;
         $this->date_naissance = new DateTime($date_naissance);
     }
-// creation getter
+    // utilisation de la fonction getAge
     public function getAge() {
         $dateCourante = new DateTime();
-        $ecart = $this->date_naissance->diff($dateCourante);
-        return $ecart->y;
+        $intervalle = $this->date_naissance->diff($dateCourante);
+        return $intervalle->y;
     }
 }
-
-$p1 = new Personne("DUPONT", "Michel", "1980-02-19");
-$p2 = new Personne("DUCHEMIN", "Alice", "1985-01-17");
-
+    //affichage des informations
 echo $p1->prenom . " " . $p1->nom . " a " . $p1->getAge() . " ans<br>";
 echo $p2->prenom . " " . $p2->nom . " a " . $p2->getAge() . " ans<br>";
 
